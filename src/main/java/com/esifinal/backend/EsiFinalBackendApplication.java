6211ead04eac048cfba03deddca570ae1d07a367
Mir Jalal Hashimli <mircelal99@gmail.com>
2022-05-22 21:50:33 +0300

    init commit

---
+++ src/main/java/com/esifinal/backend/EsiFinalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.esifinal.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsiFinalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsiFinalBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/esifinal/backend/EsiFinalBackendApplicationTests.java
@@ -0,0 +1,14 @@
+package com.esifinal.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsiFinalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
